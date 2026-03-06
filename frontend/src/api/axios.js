49fc624ab412457cbf46106b0d065e738a3a5482
tannie0299-code <tannie0299@gmail.com>
2026-03-06 15:27:20 +0530

    Created axios configuration file with baseURL and Bearer token.

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,14 @@
+'use strict';
+
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'http://localhost:8001/api',
+// Create an instance of Axios with default settings
+const api = axios.create({
+    baseURL: 'http://localhost:8001/api',
+    headers: {
+        'Authorization': `Bearer YOUR_ACCESS_TOKEN`, // replace YOUR_ACCESS_TOKEN with your actual token
+        'Content-Type': 'application/json',
+    },
 });
 
-export default instance;
+export default api;
